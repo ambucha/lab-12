aaf915d09661df16b4dd02e1eb104d6eb057d89e
ambucha <ambuchatskiy@gmail.com>
2025-09-19 23:11:34 -0700

    Initial COmmit p2

---
+++ lab-12.cpp
@@ -0,0 +1,16 @@
+// COMSC-210 | Lab 7 | Andrei Buchatskiy
+// IDE used : Visual Studio Code
+
+// Brainstorm:
+/* What is some fun data I could use to put itno an array and manipulate?
+   Given example was rainfall, as an example. THink of other numeric data that could
+   be interesting to analyze using the different <array> functions
+*/
+
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+
